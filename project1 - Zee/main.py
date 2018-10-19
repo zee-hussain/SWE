dcf5a07b23c9de3f182a1157150f824e6fc8fb4d
tracychea <31681338+tracychea@users.noreply.github.com>
2018-10-18 19:26:03 -0500

    gui stuff
    asdf

--- project1 - Zee/main.py
+++ project1 - Zee/main.py
@@ -8,6 +8,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from os.path import basename
+from tkinter import*
+
 
 account_sid = 'ACffd2fce359a976ec9b7eb4544bb503fd'
 auth_token = '8907db481180c1ff11a26faaba44746e'
@@ -18,6 +20,23 @@ email = ''
 spendLimit = ''
 loginSuccess = False
 
+
+
+def checkIsInteger(x):
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
+
+def checkTitleNoInt(x):
+    testString = str(x)
+    for i in x:
+        if i.isdigit() == True:
+            return False
+            break
+    else:
+        i+=1
 def register():
     username = input('Please enter your desired username: ')
     file = open('passwd.txt', 'r')
@@ -204,5 +223,60 @@ def mainMenu():
 
 def main():
     mainMenu()
+#don't need this function, but just incase we want to create a window, here is a good reference
+def createNewWindow():
+    recordExpense = Toplevel()
+    recordExpense.geometry("500x500")
+    Label(recordExpense, text = "Name of Item: ").grid(row = 0)
+    Label(recordExpense, text = "Price of item: ").grid(row = 1)
+    #when you create the textbox, you need to define the variable to link it to in order to retrieve it
+    nameItem = Entry(recordExpense, textvariable = nameItem_value)
+    price = Entry(recordExpense, textvariable = price_value)
 
-main()
+    nameItem.grid(row = 0, column = 1)
+    price.grid(row = 1, column = 1)
+
+    saveButton = Button(recordExpense, text = "Job's Done", fg = "Blue", bg = "Grey", command = checkIsInteger(str(price_value.get())) and recordExpense)
+    saveButton.grid(row = 2, column = 1)
+    
+    recordExpense.mainloop()
+
+#creates a blank window
+#every GUI has to have this and mainloop
+mainWindow = Tk()
+#sets size of window at start up
+mainWindow.geometry("500x500")
+#makes window unable to be resized by users
+mainWindow.resizable(width = False, height = False)
+#to populate the window with a label, create using Label(location, x)
+#pack(fill =x) button will stretch however long window is stretched (left and right)
+#replace above with y, will stretch height wise
+#fill = BOTH, expand = True, will dynamically change both x and y
+appTitle = Label(text = "Expense Calculator", bg = "Grey", fg = "White")
+#when you don't care where the object is placed, but you just want it display in window
+appTitle.pack(fill = BOTH, expand = True)
+#creating containers to place windows or widgets or objects in seperate areas of the entire screen  
+topFrame = Frame(mainWindow)
+topFrame.pack(side = TOP)
+
+bottomFrame = Frame(mainWindow)
+bottomFrame.pack(side = BOTTOM)
+
+#defining variables
+nameItem_value = StringVar()
+price_value = StringVar()
+
+#creation of button: Button(location, text, fg = (text color)) or bg = background color
+quitButton = Button(text = "Quit", fg = "blue", bg = "Grey", command = quit)
+recordButton = Button(text = "Record Expenses", fg = "red",bg= "Grey", command = createNewWindow)
+displayButton = Button(text = "Display Expenses", fg = "red",bg= "Grey", command = viewExpenses)
+#by default, pack stacks objects on top of one another. 
+#define side = to specify where on window you want
+
+recordButton.pack(in_ = topFrame, side = LEFT)
+quitButton.pack(in_ = topFrame, side = RIGHT)
+displayButton.pack(in_ = topFrame, side = LEFT)
+
+#place window in infinite loop, keeps it displayed, until you press close
+mainWindow.mainloop()
+
