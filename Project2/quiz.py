0fa50ad871cc574d3935ef1e7459f4d436e84758
Nicholasn1995 <43281775+Nicholasn1995@users.noreply.github.com>
2018-11-11 05:10:25 -0600

    Basic functionality all done

--- Project2/quiz.py
+++ Project2/quiz.py
@@ -1,54 +1,93 @@
 from pandas import DataFrame
 import pandas as pd
 import time
+import random
 
 def main():
 	#define Variables
 	score = 0
 	computerScore = 0
 	placement = 0
+	playerList =[]
 	#Host input name to make personal
-	name = input("Enter your name: ")
-	print("Welcome",name,"to the Best Game Ever")
-	#read excel file
-	fileName = r'questionList.xlsx'
+	"""name = input("Enter your name: ")
+	print("Welcome",name,"to the Best Game Ever")"""
+	#read excel file, let user decide which question list they wanna use
+	questionListName = input("Enter the name of the question file you are using.")
+	fileName = r'{}.xlsx'.format(questionListName)
 	df = pd.read_excel(fileName)
 	print(time.time())
+	
+
+	#Decide how many players are playing
+	ready = False
+	#Decide who goes first
+	while ready == False: 
+		name = input("Enter your name: ")
+		playerList.append({name: name, "Score: ": 0})
+		book = input("Do you want to add anymore players? (y/n)")
+		if book == "y":
+			continue
+		else:
+			ready = True
+	print(playerList)
+	#randomize players so everyone gets a fair chance 
+	print(len(playerList))
+	listLength = len(playerList)
+	#placeholder
+	beginning = 0
 	#goes through row by row reading questions
 	for questions in df['Questions']:
-		start = time.time()
+		print(playerList[beginning],"it is your turn. Here is your question: ")
 		print(questions)
 		#asks user to input their answer
 		userAnswer = input("Answer: ")
-		end = time.time()
-		print(end)
+		start = time.time()
 		#Check if user answer matches question answer
 		if userAnswer == df['Answers'][placement]:
 			end = time.time()
 			if end-start> 5:
 				print("Too slow, computer has answered")
+				print("1")
 				computerScore+=1
 			else:
 				print("Correct!")
 				print(df['Answers'][placement])
-				score+=1
+				playerList[beginning]["Score: "] +=1
 			placement+=1
 		else:
 			if end-start> 5:
 				print("Too slow, computer has answered")
+				print("2")
 				computerScore+=1
 			else:
 				print("Incorrect")
 				print("Correct answer is: ",df['Answers'][placement])
 			placement+=1
-
+		if beginning != len(playerList) -1:
+			beginning+=1
+		else:
+			beginning = 0
 	print("Game Over.")
-	print("The final score is: ")
-	print(name,":",score,"       ","Computer:",computerScore)
-	if score>computerScore:
-		print("You win!!!")
-	if score<computerScore:
-		print("Try again next time")
+	print("The final score are: ")
+	for i in playerList:
+		print(playerList)
+	#finding winner
+	highestScore = 0
+	winnerList = []
+	for j in range(0,len(playerList)):
+		if int(playerList[j]["Score: "]) > highestScore:
+			print(playerList[j]["Score: "])
+			winnerList.clear()
+			highestScore = int(playerList[j]["Score: "])
+			winnerList.append(playerList[j])
+		elif int(playerList[j]["Score: "]) == highestScore:
+			print(playerList[j]["Score: "])
+			winnerList.append(playerList[j])
+		else:
+			continue
+	if len(winnerList) > 1:
+		print("It was a tie between: ",winnerList)
 	else:
-		print("TIE!!!")
+		print("The winners is(are): ",winnerList)
 main()
